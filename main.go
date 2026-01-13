233b9999ac264b0f6fc4693912ac9f2f89d46f86
Throne-of-Doom <cg-86@live.com>
2026-01-13 07:44:35 -0500

    Update main.go and add assets directory

--- main.go
+++ main.go
@@ -6,9 +6,16 @@ import (
 
 const filepathRoot = "."
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	srv := &http.Server{
 		Addr:    ":8080",
